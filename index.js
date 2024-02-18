82f58a38ed812fc562929c3b5175ba6b1855c1d3
crontreras1 <crontreras1@gmail.com>
2024-02-17 19:01:54 -0500

    class 7. GET method

--- index.js
+++ index.js
@@ -11,9 +11,34 @@ app.get('/nueva-ruta', (req, res) => {
 })
 
 app.get('/productos', (req, res) => {
+  res.json([
+    {
+      name: 'Producto 1',
+      price: 1000
+    },
+    {
+      name: 'Producto 2',
+      price: 1500
+    }
+  ])
+})
+
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params
+
   res.json({
-    name: 'Producto 1',
-    price: 1000
+    id,
+    name: 'Producto 2',
+    price: 1500
+  })
+})
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params
+
+  res.json({
+    categoryId,
+    productId
   })
 })
 
