a9a63ac2c3295d927395851aa76b4dfa8247c1df
crontreras1 <crontreras1@gmail.com>
2024-03-03 14:20:27 -0500

    class 16. async await and try catch

--- services/product.service.js
+++ services/product.service.js
@@ -6,7 +6,7 @@ class ProductsService {
     this.generate()
   }
 
-  generate () {
+  async generate () {
     const limit = 100
 
     for (let index = 0; index < limit; index++) {
@@ -19,7 +19,7 @@ class ProductsService {
     }
   }
 
-  create (data) {
+  async create (data) {
     const newProduct = {
       id: faker.datatype.uuid(),
       ...data
@@ -30,15 +30,19 @@ class ProductsService {
     return newProduct
   }
 
-  find () {
-    return this.products
+  async find () {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(this.products)
+      }, 5000)
+    })
   }
 
-  findOne (id) {
+  async findOne (id) {
     return this.products.find(item => item.id === id)
   }
 
-  update (id, changes) {
+  async update (id, changes) {
     const index = this.products.findIndex(item => item.id === id)
 
     if (index === -1) {
@@ -54,7 +58,7 @@ class ProductsService {
     return this.products[index]
   }
 
-  delete (id) {
+  async delete (id) {
     const index = this.products.findIndex(item => item.id === id)
 
     if (index === -1) {
