cb86350b8c03daa9b5c90a80f4af564bb41470fa
crontreras1 <crontreras1@gmail.com>
2024-03-10 17:56:46 -0500

    class 21. testing endpoints

--- routes/products.router.js
+++ routes/products.router.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const ProductsService = require('../services/product.service')
+const validatorHandler = require('../middleware/validator.handler')
+const { createProductSchema, updateProductSchema, getProductSchema} = require('../schemas/product.schema')
 
 const router = express.Router()
 const service = new ProductsService()
@@ -10,35 +12,48 @@ router.get('/', async (req, res) => {
   res.json(products)
 })
 
-router.get('/:id', async (req, res, next) => {
-  try {
-    const { id } = req.params
-    const product = await service.findOne(id)
+router.get('/:id',
+  validatorHandler(getProductSchema, 'params'),
 
-    res.json(product)
-  } catch (error) {
-    next(error)
+  async (req, res, next) => {
+    try {
+      const { id } = req.params
+      const product = await service.findOne(id)
+
+      res.json(product)
+    } catch (error) {
+      next(error)
+    }
   }
-})
+)
 
-router.post('/', async (req, res) => {
-  const body = req.body
-  const newProduct = await service.create(body)
+router.post('/',
+  validatorHandler(createProductSchema, 'body'),
 
-  res.status(201).json(newProduct)
-})
-
-router.patch('/:id', async (req, res, next) => {
-  try {
-    const { id } = req.params
+  async (req, res) => {
     const body = req.body
-    const product = await service.update(id, body)
+    const newProduct = await service.create(body)
 
-    res.json(product)
-  } catch (error) {
-    next(error)
+    res.status(201).json(newProduct)
   }
-})
+)
+
+router.patch('/:id',
+  validatorHandler(getProductSchema, 'params'),
+  validatorHandler(updateProductSchema, 'body'),
+
+  async (req, res, next) => {
+    try {
+      const { id } = req.params
+      const body = req.body
+      const product = await service.update(id, body)
+
+      res.json(product)
+    } catch (error) {
+      next(error)
+    }
+  }
+)
 
 router.delete('/:id', async (req, res) => {
   const { id } = req.params
